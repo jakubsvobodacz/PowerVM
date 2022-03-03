e4a097726fe80c385a4b959b27c96a96ae79d095
jasvd <jasvd@leo-pharma.com>
2022-03-03 21:34:50 +0100

    proxies added

--- StopVM/run.ps1
+++ StopVM/run.ps1
@@ -6,7 +6,23 @@ param($Request, $TriggerMetadata)
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
 
+
 # Interact with query parameters or the body of the request.
+Write-Output ($request | ConvertTo-Json -depth 99)
+
+$ResourceGroupName = $request.query.ResourceGroupName
+$VMName = $request.query.VMName
+$Context =  $request.query.Context
+$Action = $request.query.Action
+
+$null = Connect-AzAccount -Identity
+$null = Set-AzContext $Context
+
+$vmStatus = Get-AzVM -ResourceGroupName $ResourceGroupName -Name $VMName -Status
+Write-output $vmStatus
+
+
+<#
 $name = $Request.Query.Name
 if (-not $name) {
     $name = $Request.Body.Name
@@ -17,6 +33,7 @@ $body = "This HTTP triggered function executed successfully. Pass a name in the 
 if ($name) {
     $body = "Hello, $name. This HTTP triggered function executed successfully."
 }
+#>
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
